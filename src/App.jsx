00fc9abc2620df032139d4365824f0e9d0c5f863
Manmeet Dhingra <manmeetdhingra@googlemail.com>
2024-10-14 17:19:15 +0100

    !footer

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
                 <Route path='/contact' element={<Contact />}/>
             </Routes>
         </Router>
-        <Footer />
+        {/* <Footer /> */}
     </main>
   )
 }
